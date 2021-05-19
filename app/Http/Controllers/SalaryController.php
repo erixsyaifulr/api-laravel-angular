8ed8c99ddae4e0c8929325c120574a564f974d0d
erixsyaifulr <erix.rohman@lidahbuayagroup.com>
2021-05-19 16:29:50 +0700

    upload file

---
+++ app/Http/Controllers/SalaryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Salary;
+
+class SalaryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Salary::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Salary::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Salary::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $salary = Salary::findOrFail($id);
+        $salary->update($request->all());
+
+        return $salary;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $salary = Salary::findOrFail($id);
+        $salary->delete();
+ 
+        return 204;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::get('employees', 'EmployeeController@index');
+Route::get('employee/{id}', 'EmployeeController@show');
+Route::post('employee', 'EmployeeController@store');
+Route::put('employee/{id}', 'EmployeeController@update');
+Route::delete('employee/{id}', 'EmployeeController@destroy');
+
+Route::get('salarys', 'SalaryController@index');
+Route::get('salary/{id}', 'SalaryController@show');
+Route::post('salary', 'SalaryController@store');
+Route::put('salary/{id}', 'SalaryController@update');
+Route::delete('salary/{id}', 'SalaryController@destroy');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
